726332ad8c71f41ad4d7acd0db4df2bcab7de9c3
JustEddie <skdpwls830@gmail.com>
2022-07-21 01:08:05 +1000

    add palindromeChecker and primeCheck functions and tests

--- phase-1/what-are-tests.js
+++ phase-1/what-are-tests.js
@@ -11,5 +11,40 @@ function add(x, y) {
 function subtract(x, y) {
     return x - y;
 }
+function primeCheck(n) {
+    let isPrime = true;
+    if (n===1){
+        return "1 is neither prime nor composite number.";
+    } else if (n>1) {
+        for(let i=2; i<n; i++){
+            if(n%i===0){
+                isPrime = false;
+                break;
+            }
+        } if(isPrime) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (n<0) {
+        return console.error("Enter a positive number.");
+    }
+}
 
-module.exports = add;
+function palindromeChecker(str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
+  }
+
+
+
+
+// module.exports = add;
+// module.exports = palindromeChecker;
+// module.exports = primeCheck;
+
+module.exports = {add, palindromeChecker, primeCheck};
+
+console.log(palindromeChecker("A man, a plan, a canal. Panama"));
--- phase-1/what-are-tests.test.js
+++ phase-1/what-are-tests.test.js
@@ -2,22 +2,87 @@
     2 + 3 = 5
     6 + 7 = 13
  */
-
-const add = require("./what-are-tests");
+const myFunctions = require("./what-are-tests")
+// const add = require("./what-are-tests");
+// const primeCheck = require("./what-are-tests");
+// const palindromeChecker = require("./what-are-tests");
 
 describe('Add', () => {
     it('should return 5 when 2, 3 is input', () => {
         const expected = 5;
-        const actual = add(2,3);
+        const actual = myFunctions.add(2,3);
 
         expect(actual).toEqual(expected);
     })
 
     it('should return 13 when 6, 7 is input', () => {
         const expected = 13;
-        const actual = add(6,7);
+        const actual = myFunctions.add(6,7);
 
         expect(actual).toEqual(expected);
     })
 });
 
+
+
+describe("PrimeCheck", ()=> {
+    it("should return true when 5 is input", () => {
+        const expected = true;
+        const actual = myFunctions.primeCheck(5);
+
+        expect(actual).toEqual(expected);
+    })
+
+    it("should return false when 4 is input", () => {
+        const expected = false;
+        const actual = myFunctions.primeCheck(4);
+
+        expect(actual).toEqual(expected);
+    })
+
+    it("should throw an error when -1 is input", () => {
+        const actual = myFunctions.primeCheck(-1);
+
+        expect(actual).toThrowError;
+    })
+
+    it("should return  1 is neither prime or composite number message when 1 is input", ()=>{
+        const actual = myFunctions.primeCheck(1);
+        expect(actual).toMatch("1 is neither prime nor composite number.");
+    })
+
+    
+})
+
+
+describe('palindromeChecker', () => {
+    it('should return true when race car is input', () => {
+        const expected = true;
+        const actual = myFunctions.palindromeChecker("eye");
+
+        expect(actual).toEqual(expected);
+    })
+
+    it('should return true when “A man, a plan, a canal. Panama” is input', () => {
+        const expected = true;
+        const actual = myFunctions.palindromeChecker("A man, a plan, a canal. Panama");
+
+        expect(actual).toEqual(expected);
+    })
+
+    it('should return true when “0_0 (: /-\ :) 0–0” is input', () => {
+        const expected = true;
+        const actual = myFunctions.palindromeChecker("0_0 (: /-\ :) 0–0");
+
+        expect(actual).toEqual(expected);
+    })
+
+    it('should return false when “not a palindrome” is input', () => {
+        const expected = false;
+        const actual = myFunctions.palindromeChecker("not a palindrome");
+
+        expect(actual).toEqual(expected);
+    })
+
+
+});
